f5cc3f6078dfb599fbddd1e42d5ae901b8f7d3fa
Tyler Berrett <btylerberrett@gmail.com>
2019-11-01 10:33:08 -0600

    add search functionality to display the Makeup items add full functionality to to searching and displaying makeup items

--- app/src/main/java/com/tylerb/makeupsearch/MainActivity.kt
+++ app/src/main/java/com/tylerb/makeupsearch/MainActivity.kt
@@ -2,15 +2,25 @@ package com.tylerb.makeupsearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import com.tylerb.makeupsearch.model.Makeup
 import com.tylerb.makeupsearch.retrofit.ApiCall
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.card_view.*
+import kotlinx.android.synthetic.main.card_view.view.*
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
-
 
 
     @Inject
@@ -18,26 +28,79 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var disposable: Disposable
 
+    var list = ArrayList<Makeup>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         (application as App).makeupComponent.inject(this)
 
-        disposable = daggerApi.getMakeupBrand("maybelline")
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(
-                {makeup: List<Makeup> -> println(makeup[0])},
-                {fail -> println(fail.message)}
-            )
+        val adapter = MakeupAdapter(list)
 
+        search_bar.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(search: String?): Boolean {
+                loading.visibility = View.VISIBLE
+                search?.let {
+                    disposable = daggerApi.getMakeupBrand(it)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(
+                            { makeup: List<Makeup> ->
+                                loading.visibility = View.INVISIBLE
+                                list.addAll(makeup)
+                                adapter.notifyDataSetChanged()
+                            },
+                            { fail ->
+                                println(fail.message)
+                                loading.visibility = View.INVISIBLE
+                            }
+                        )
+                }
+
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                if (p0?.length == 0){
+                    list.clear()
+                    adapter.notifyDataSetChanged()
+                }
+                return true
+            }
+
+        })
+
+        recycler_view.layoutManager = LinearLayoutManager(this)
+        recycler_view.adapter = adapter
 
     }
 
+    inner class MakeupAdapter(private val list: ArrayList<Makeup>) :
+        RecyclerView.Adapter<MakeupAdapter.ViewHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view =
+                LayoutInflater.from(parent.context).inflate(R.layout.card_view, parent, false)
+            return ViewHolder(view)
+        }
 
-    
+        override fun getItemCount() = list.size
 
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val item = list[position]
+            Picasso.get().load(item.image_link).into(holder.image)
+            holder.name.text = item.name
+            holder.price.text = item.price
+            holder.rating.text = item.rating
+        }
+
+        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val image = view.makeup_image
+            val name = view.makeup_name
+            val price = view.makeup_price
+            val rating = view.makeup_rating
+        }
+    }
 
 
     override fun onDestroy() {
